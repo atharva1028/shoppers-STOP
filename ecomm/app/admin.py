6fcae873268b781c35fa5d838b608ce085ede8a4
atharva1028 <satankaratharva@gmail.com>
2023-05-22 13:26:08 +0530

    i have to add models in over projects

--- ecomm/app/admin.py
+++ ecomm/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . models import (
+    Customer,
+    Product,
+    Cart,
+    OrderPlaced
+)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(OrderPlaced)
 
 # Register your models here.
